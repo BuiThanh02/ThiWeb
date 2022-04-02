64639e7eae6ea2980bbdbf58ff98fd83d935d402
Vuzuize <vucute775@gmail.com>
2022-04-02 17:18:18 +0700

    chinh sua chiotiet

--- view/main.js
+++ view/main.js
@@ -1,13 +1,22 @@
+/*const toggle= document.getElementById('toggle');
+toggle.onclick = function(){
+   toggle.classList.toggle('active');
+}*/
+
+const playpause = document.querySelector('.playpause');
+
+playpause.addEventListener('click', () => {
+  playpause.classList.toggle('playing');
+});
+
+
 var mySong =document.getElementById("mySong");
 var icon_play =document.getElementById("icon_play");
 
 icon_play.onclick = function(){
     if(mySong.paused){
         mySong.play();
-        icon_play.setAttribute("scr", "pause.png");
-
     }else{
         mySong.pause();
-        icon_play.setAttribute("scr", "play.button.png");
     }
 }
